96a8ffc96c8434cedcecf1ab8f5341351f257daa
hassanysh <137109343+hassanys@users.noreply.github.com>
2023-11-21 10:48:22 +0000

    Function for a single round defined

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ console.log("Hello World")
 
 
 // step 1 generating random number 
-function get (){
+function getComputerChoice (){
     let input = Math.floor(Math.random() *3);
 
     if (input === 0){
@@ -19,4 +19,52 @@ function get (){
     }
 
 }
+console.log(getComputerChoice())
+
+//  step 2: define function for a single round
+   
+const playerSelection = "rock".toLowerCase();
+const computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  if (playerSelection === computerSelection){
+     return "draw"
+  }
+
+  else if (playerSelection === "rock" && computerSelection === "Paper"){
+    return "You win! Paper beats Rock"
+  }
+
+  else {
+    return "You Lose! Paper beats Rock"
+  }
+}
+
+console.log(playRound(playerSelection,computerSelection))
+
+
+
+
+
+
+
+// function playRound(playerSelection, computerSelection {
+
+// const playerScore = 0
+// const computerScore = 0
+//      if (playerSelection === computerSelection) {
+//            console.log (Draw")
+//     }  
+
+//     else if  ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+//     (playerSelection === "Paper" && computerSelection === "Rock") ||
+//     (playerSelection === "Scissors" && computerSelection === "Paper")
+// )   {
+//     computerScore++
+//     console.log("You win")
+//     }
+    
+
 
